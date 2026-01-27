e80c4b204759315f233d16a11fb92ab322b7eff1
kithogun666dunnel <pankajkumarrko05@gmail.com>
2026-01-28 00:13:55 +0530

    chore: add empty ReactionDispatcher boundary

---
+++ src/reactions/dispatcher.ts
@@ -0,0 +1,12 @@
+// src/reactions/dispatcher.ts
+
+export class ReactionDispatcher {
+  constructor() {
+    // intentionally empty
+  }
+
+  async dispatch(event: any): Promise<void> {
+    // reactions will be added later
+    return
+  }
+}
